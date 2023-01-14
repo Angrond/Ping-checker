20ff72e94b81a8eb898deffa9a61d00a41fb08eb
Angrond <122674882+Angrond@users.noreply.github.com>
2023-01-14 14:07:56 +0100

    Add files via upload

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.stopwatch = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ido = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.szazalek = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Felmérés indítása";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Felmérés leállítása";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(2, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 137);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(72, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "00";
+            // 
+            // stopwatch
+            // 
+            this.stopwatch.Enabled = true;
+            this.stopwatch.Interval = 10;
+            this.stopwatch.Tick += new System.EventHandler(this.stopwatch_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(240, 179);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(291, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // ido
+            // 
+            this.ido.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ido.Location = new System.Drawing.Point(240, 117);
+            this.ido.Name = "ido";
+            this.ido.Size = new System.Drawing.Size(303, 59);
+            this.ido.TabIndex = 13;
+            this.ido.Text = "00:00:00:000";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(240, 52);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1440,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(291, 38);
+            this.numericUpDown1.TabIndex = 14;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(240, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 40);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Milyen hosszú legyen?";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(537, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "perc";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(562, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "label5";
+            // 
+            // szazalek
+            // 
+            this.szazalek.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.szazalek.Location = new System.Drawing.Point(12, 244);
+            this.szazalek.Name = "szazalek";
+            this.szazalek.Size = new System.Drawing.Size(157, 132);
+            this.szazalek.TabIndex = 18;
+            this.szazalek.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 385);
+            this.Controls.Add(this.szazalek);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.ido);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer stopwatch;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label ido;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label szazalek;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        int elkuldott = 0;
+        int sikeres = 0;
+        private Stopwatch stopWatch=new Stopwatch();
+        int hossz = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer2 = new Timer();
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Interval = 500; // in miliseconds
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            string fajlnev = DateTime.Today.Year.ToString() + DateTime.Today.Month.ToString() + DateTime.Today.Day.ToString() + ".txt";
+            elkuldott++;
+            try
+            {
+                Ping myPing = new Ping();
+                PingReply reply = myPing.Send("google.com", 1000);
+                if (reply != null)
+                {
+                    label3.Text = reply.RoundtripTime.ToString();
+
+                }
+                if (reply.RoundtripTime > 200)
+                {
+                    StreamWriter sw = new StreamWriter(fajlnev, true);
+                    sw.WriteLine(DateTime.Now.ToString() + " Latency: " + reply.RoundtripTime.ToString());
+                    sw.Close();
+                    label2.Text = "!";
+                }
+                else 
+                {
+                    label2.Text = "";
+                    sikeres++;
+                }
+            }
+            catch
+            {
+                
+                
+                StreamWriter sw = new StreamWriter(fajlnev, true);
+                sw.WriteLine(DateTime.Now.ToString() + " unable to ping");
+                sw.Close();
+                label2.Text = "!";
+            }
+            double szazalekki = Convert.ToDouble(sikeres) / Convert.ToDouble(elkuldott) * 100;
+            szazalek.Text = sikeres.ToString() + " / " + elkuldott.ToString() +" "+ szazalekki.ToString("#0.##")+"%";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hossz = Convert.ToInt32(numericUpDown1.Value);
+            timer2.Start();
+            stopWatch.Reset();
+            stopWatch.Start();
+            progressBar1.Maximum = hossz * 60000+100;
+            progressBar1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            stopWatch.Stop();
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopwatch_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = Convert.ToInt32(stopWatch.ElapsedMilliseconds);
+            if (progressBar1.Value>=hossz*60000)
+            {
+                progressBar1.Visible = false;
+            }
+            if (stopWatch.ElapsedMilliseconds >= hossz*60000)
+            {
+                button2_Click(sender, e);
+            }
+            this.ido.Text = string.Format("{0:hh\\:mm\\:ss\\:fff}", stopWatch.Elapsed);
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
